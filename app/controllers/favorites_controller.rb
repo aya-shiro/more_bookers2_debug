2fecff542d1f61f8c82bd9e4d8fc655e20a895af
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-03-03 11:30:32 +0000

    [update+u]b2-debug-main

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
   def create
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
     # 外部キーbook_idに紐づいたbookテーブルの1レコードbook.idをもとにインスタンス生成し、いいねを作る
     favorite = current_user.favorites.new(book_id: book.id)
@@ -7,17 +8,20 @@ class FavoritesController < ApplicationController
 
     # redirect_backでこのアクションを使ったときの画面に戻る！
     # 引数で指定しているroot_pathはredirect_backできなかったときの緊急避難先、root_pathが無難
-    redirect_back(fallback_location: root_path)
+    # 課題6非同期通信のため削除
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
     # createで作ったインスタンスをfind_byで取得してdestroyする
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
 
     # いいねを押す前にいた画面をリダイレクト先に
-    redirect_back(fallback_location: root_path)
+    # 課題6非同期通信のため削除
+    # redirect_back(fallback_location: root_path)
 
   end
 
